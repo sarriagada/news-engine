06857c8333625db81b2c0bb89ec3d34f32db2761
Sergio Sebastian Arriagada <sarriagada87@gmail.com>
2022-06-01 13:50:11 -0300

    add simplecov

--- Gemfile
+++ Gemfile
@@ -77,3 +77,5 @@ gem "rubocop", require: false
 gem "rubocop-rails", require: false
 gem "rubocop-performance", require: false
 gem "rubocop-rspec", require: false
+
+gem "simplecov", require: false, group: :test
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,6 +2,16 @@ ENV["RAILS_ENV"] ||= "test"
 require_relative "../config/environment"
 require "rails/test_help"
 
+if ENV["RAILS_ENV"] == "test"
+  require "simplecov"
+  SimpleCov.start :rails do
+    add_filter "app/models/application_record.rb"
+    add_filter "app/channels/application_cable/channel.rb"
+    add_filter "app/channels/application_cable/connection.rb"
+  end
+  puts "required simplecov"
+end
+
 class ActiveSupport::TestCase
   # Run tests in parallel with specified workers
   parallelize(workers: :number_of_processors)
