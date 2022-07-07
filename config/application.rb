1967da81c8d2ada19b729577bbfbaf311673f091
Sergio Sebastian Arriagada <sarriagada87@gmail.com>
2022-07-07 16:40:40 -0300

    Update application.rb

--- config/application.rb
+++ config/application.rb
@@ -7,7 +7,7 @@ require "rails/all"
 Bundler.require(*Rails.groups)
 
 # Load dotenv only in development or test environment
-if ['development', 'test'].include? ENV['RAILS_ENV']
+if %w[development test].include? ENV["RAILS_ENV"]
   Dotenv::Railtie.load
 end
 
