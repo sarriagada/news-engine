cd87cf2b9509caa678461a78e5c0de2585488860
gbarazzutti <gbarazzutti2@gmail.com>
2022-09-15 10:32:21 -0300

    rspec added

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: "home#index"
-  get 'admin/dashboard' => 'admin/dashboard#index'
+  get "admin/dashboard" => "admin/dashboard#index"
   namespace :admin do
     root to: "dashboard#index"
     resources :articles
