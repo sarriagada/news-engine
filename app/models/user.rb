51e266d7cbae609ebf5e270f8b865df3fdac5340
gbarazzutti <gbarazzutti2@gmail.com>
2022-09-22 11:21:08 -0300

    user avatar added

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,6 @@ class User < ActiveRecord::Base
   validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
 
   validates :email, uniqueness: true
+
+  has_one_attached :avatar
 end
