c35b400ad32de55416c0c6c8f32896d41666258c
gbarazzutti <gbarazzutti2@gmail.com>
2022-10-05 15:12:55 -0300

    bugs fixed

--- db/schema.rb
+++ db/schema.rb
@@ -65,7 +65,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_23_144430) do
     t.string "email", null: false
     t.string "crypted_password"
     t.string "salt"
-    t.integer :role,               null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
